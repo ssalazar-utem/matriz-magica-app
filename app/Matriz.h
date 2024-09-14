62abe637320c19b6e8f831c3af6d5abdd7662af6
Sebastián Salazar Molina <sebasalazar@gmail.com>
2024-09-14 11:10:38 -0300

    Agregamos clase matriz dinámica impar

---
+++ app/Matriz.h
@@ -0,0 +1,64 @@
+#ifndef MATRIZ_H
+#define MATRIZ_H
+
+#include <iostream>
+#include <iomanip>
+
+class Matriz {
+public:
+    /**
+     * Este constructor necesita un valor impar mayor que cero.
+     * @param largo Indica el tamaño de la matriz
+     */
+    Matriz(int largo);
+
+    /**
+     * Asigna a nuestra matriz los valores referenciados por la matriz del argumento.
+     * @param orig Matriz de copia
+     */
+    Matriz(const Matriz& orig);
+
+    /**
+     * Destructor que libera la memoria usada por la matriz.
+     */
+    virtual ~Matriz();
+
+    /**
+     * Imprime nuestra matriz en pantalla
+     */
+    void imprimir() const;
+
+    /**
+     * 
+     * @return el tamaño de la matriz
+     */
+    int GetLargo() const;
+
+    /**
+     * 
+     * @param fila Fila a buscar
+     * @param columna Columna a buscar
+     * @param valor Valor a modificar
+     */
+    void SetValor(int fila, int columna, int valor);
+
+    /**
+     * 
+     * @param fila Fila a buscar
+     * @param columna Columna a buscar
+     * @return el valor de la posición indicada
+     */
+    int GetValor(int fila, int columna) const;
+
+    /**
+     * 
+     * @return true si la matriz es mágica o false en cualquier otro caso
+     */
+    bool esMagica() const;
+private:
+    int** datos;
+    int largo;
+};
+
+#endif /* MATRIZ_H */
+
--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include "Matriz.h"
 
 
 /**
@@ -10,19 +11,41 @@
 void participantes(std::string programa);
 
 /**
+ * 
+ * @param min Valor mínimo
+ * @param max Valor máximo
+ * @return un número aleatoreo entre los valores del argumento
+ */
+int aleatoreo(int min, int max);
+
+/**
  * Taller computacional
  * @param argc cantidad de argumentos
  * @param argv argumentos
  * @return El código de salida del programa
  */
 int main(int argc, char** argv) {
-
+    srand(time(0));
     /**
      * Incluir acá la lógica del programa
      * 
      */
     if (argc > 1) {
-        // @TODO programar proyecto
+        int largo = std::stoi(argv[1]);
+
+        Matriz magica(largo);
+
+        for (int fila = 0; fila < largo; fila++) {
+            for (int columna = 0; columna < largo; columna++) {
+                int valor = aleatoreo(1, 99);
+                magica.SetValor(fila, columna, valor);
+            }
+        }
+
+        magica.imprimir();
+        std::cout << std::endl << "La matriz es magica " << magica.esMagica() << std::endl;
+
+
     } else {
         // Mostrar los integrantes
         participantes(argv[0]);
@@ -31,8 +54,11 @@ int main(int argc, char** argv) {
 }
 
 void participantes(std::string programa) {
-    std::cout << std::endl << "=== Taller " << programa << " ===" << std::endl;
-    std::cout << std::endl << "Juanito"; // Reemplazar por su nombre
-    std::cout << std::endl << "Pedrito"; // Reemplazar por su nombre
-    std::cout << std::endl << "Dieguito" << std::endl; // Reemplazar por su nombre
+    std::cout << std::endl << "=== Programa de ejemplo " << programa << " ===" << std::endl;
+    std::cout << std::endl << "Sebastián Salazar Molina"; // Reemplazar por su nombre
+    std::cout << std::endl << "14/09/2024 :: Sólo uso académico." << std::endl; // Reemplazar por su nombre
+}
+
+int aleatoreo(int min, int max) {
+    return (rand() % (max - min + 1)) +min;
 }
